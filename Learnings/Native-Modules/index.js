756e4d415de86ecd075ef5827a8916b26c591642
Saksham67 <sakshamkumar652547@gmail.com>
2025-05-24 16:57:43 +0530

    updated index.js

--- Learnings/Native-Modules/index.js
+++ Learnings/Native-Modules/index.js
@@ -1,3 +1,4 @@
 const circle = require('./circle')
 
 console.log('Area of a circle with radius 7 is: ', circle.area(7));
+console.log('Circumference of a circle with radius 7 is: ', circle.circumference(7));
