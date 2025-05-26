f8375b3cd8add3a996f1ba84835fe5fb6609c6b5
Saksham67 <sakshamkumar652547@gmail.com>
2025-05-27 01:22:38 +0530

    modified server.js file

--- Learnings/server.js
+++ Learnings/server.js
@@ -1,4 +1,7 @@
 const http = require('http');
-const server = http.createserver((req, res) => {
+const server = http.createServer((req, res) => {
     res.end('Server running successfully!');
+});
+server.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
 })
