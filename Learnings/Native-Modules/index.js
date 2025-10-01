acc29113cfa76addea398ce02f8b2d7962d8980f
Saksham67 <sakshamkumar652547@gmail.com>
2025-10-01 15:05:16 +0530

    deleted native modules directory

--- Learnings/Native-Modules/index.js
+++
@@ -1,4 +0,0 @@
-const circle = require('./circle')
-
-console.log('Area of a circle with radius 7 is: ', circle.area(7));
-console.log('Circumference of a circle with radius 7 is: ', circle.circumference(7));
